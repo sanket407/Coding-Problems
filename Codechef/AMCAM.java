afca213d4c13b07f5bacc84b11e7229fd82174d3
MaNIaC <sanket407@gmail.com>
2015-12-27 16:10:39 +0530

    #bipartite matching #medium

--- Codechef/AMCAM.java
+++ Codechef/AMCAM.java
@@ -1,3 +1,26 @@
+//CODECHEF https://www.codechef.com/AMR15MOS/problems/AMCAM
+//MEDIUM
+//BIPARTITE MATCHING + BINARY SEARCH
+//FIRST CALCULATE ALL POSSIBLE n*n distances and store them in sorted array DIST
+//Now we choose each value of DIST as our ANS and find if it is possible to get k animals  if that value is ANSs
+//Instead of linear searching we binary search on the array . 
+//Suppose we start at mid = (low+high) /2
+//If our chosen dist gives no. of animals less than k that means the limit should be decreased. so high=mid-1
+//If our chosen dist gives no, of animals greater than k that means the limit should be increased. so low=mid+1
+
+//To find no. of animals who satisfy  a particular limit :
+//Consider cats and mice as a bipartite graph with 2 sets
+//We draw an edge between a cat and mouse only if dist. betwn them is GREATER than limit
+//Since  the animals at edge end pts  dont satisfy limit we remove any one of them
+//Thus after creating the graph with edges , the max no of animals which can be removed = Max Independent Set of the graph/
+//MID= no of vertices - Min Vertex Cover =2*n -Min Vertex Cover
+//But in bipartite graph min vertex cover == Maximum bipartite matching (by konigs theorem)
+//Find max bipartite matching using augmented path algo ref: http://www.slideshare.net/KuoE0/acmicpc-bipartite-matching
+
+
+
+
+
 import java.io.*;
 import java.util.*;
  
@@ -49,33 +72,48 @@ public class Main{
        
        int low=0,high=n*n-1,mid=(high+low)/2;
        
-       while(true)
-       {
-          double limit =d[mid];
+       while(low<=high)
+       {mid=(high+low)/2;
           
-           adj=new int[n][n];
-           
+       double limit =d[mid];
+          
+           adj=new int[2*n][2*n];
+          
           
           for(int i=0;i<n;i++)
               for(int j=0;j<n;j++)
               {
                   if(dist[i][j]>limit)
-                      adj[i][j]=1;
+                  {adj[i][n+j]=1;adj[n+j][i]=1;}
                   
               }
-           
-          matched=new int[n];
+          
+          
+          matched=new int[2*n];
           maxmatch=new int [n][n];
           for(int i=0;i<n;i++)
-          {int visited[]=new int[n];
+          {int visited[]=new int[2*n];
               boolean a=find_aug_path(i,visited);
           }           
-           
-           
+            c=0;
+          for(int i=0;i<n;i++)
+              if(matched[i]==1)
+                  c++;
+        
+          if((2*n-c)<k)
+          {
+             low=mid+1;
+          }
+          else if((2*n-c)>k)
+          {
+              high=mid-1;
+              
+          }
+          else break;
            
            
        }
-       
+        System.out.println(d[mid]);
         
         
     }
@@ -85,9 +123,9 @@ public static boolean find_aug_path(int curr,int visited[])
 {
     for(int i=0;i<n;i++)
     {
-        if(adj[curr][i]==1 && visited[i]!=0)
-        {  visited[i]=1;
-            int next=i;
+        if(adj[curr][n+i]==1 && visited[n+i]==0)
+        {  visited[n+i]=1;visited[curr]=1;
+            int next=n+i;
             
             if(matched[next]==0 || find_aug_path(next,visited))
             {   
