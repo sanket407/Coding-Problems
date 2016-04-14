41d908d4947536817e5dc0bb4d0357c3c665ecad
sanket407 <sanket407@gmail.com>
2016-04-14 15:42:28 +0530

    #easy-dp

---
+++ CodeForces/Boredom.java
@@ -0,0 +1,85 @@
+// http://codeforces.com/problemset/problem/455/A
+
+
+import java.io.*;
+ 
+import java.util.*;
+ 
+public class Main {
+	
+	public static long dp[],c[];
+	public static int mod = 1000000007;
+	
+	public static void main(String[] args)throws IOException {
+		
+OutputWriter out=new OutputWriter(System.out);
+  Reader r=new Reader();
+ BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+ 
+ int n=r.nextInt();
+ 
+ c=new long[100001];
+ for(int i=0;i<n;i++)
+	 c[r.nextInt()]++;
+ dp=new long[100001];
+ Arrays.fill(dp,-1);
+ dp[0]=0;
+ dp[1]=c[1]*1;
+ 
+ for(int i=2;i<=100000;i++)
+ {
+	 dp[i]=Math.max( dp[i-1] , dp[i-2] + c[i]*i );
+	 
+	 
+ }
+ 
+ System.out.println(dp[100000]);
+ 
+ 
+	}
+	
+
+}
+ 
+class Reader {
+    final private int BUFFER_SIZE = 1 << 16;private DataInputStream din;private byte[] buffer;private int bufferPointer, bytesRead;
+    public Reader(){din=new DataInputStream(System.in);buffer=new byte[BUFFER_SIZE];bufferPointer=bytesRead=0;
+    }public Reader(String file_name) throws IOException{din=new DataInputStream(new FileInputStream(file_name));buffer=new byte[BUFFER_SIZE];bufferPointer=bytesRead=0;
+    }public String readLine() throws IOException{byte[] buf=new byte[64];int cnt=0,c;while((c=read())!=-1){if(c=='\n')break;buf[cnt++]=(byte)c;}return new String(buf,0,cnt);
+    }public int nextInt() throws IOException{int ret=0;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');if(neg)c=read();do{ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(neg)return -ret;return ret;
+    }public long nextLong() throws IOException{long ret=0;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');if(neg)c=read();do{ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(neg)return -ret;return ret;
+    }public double nextDouble() throws IOException{double ret=0,div=1;byte c=read();while(c<=' ')c=read();boolean neg=(c=='-');if(neg)c = read();do {ret=ret*10+c-'0';}while((c=read())>='0'&&c<='9');if(c=='.')while((c=read())>='0'&&c<='9')ret+=(c-'0')/(div*=10);if(neg)return -ret;return ret;
+    }private void fillBuffer() throws IOException{bytesRead=din.read(buffer,bufferPointer=0,BUFFER_SIZE);if(bytesRead==-1)buffer[0]=-1;
+    }private byte read() throws IOException{if(bufferPointer==bytesRead)fillBuffer();return buffer[bufferPointer++];
+    }public void close() throws IOException{if(din==null) return;din.close();}
+}
+ 
+class OutputWriter {
+	private final PrintWriter writer;
+ 
+	public OutputWriter(OutputStream outputStream) {
+		writer = new PrintWriter(outputStream);
+	}
+ 
+	public OutputWriter(Writer writer) {
+		this.writer = new PrintWriter(writer);
+	}
+ 
+	public void print(Object... objects) {
+		for (int i = 0; i < objects.length; i++) {
+			if (i != 0)
+				writer.print(' ');
+			writer.print(objects[i]);
+		}
+	}
+ 
+	public void printLine(Object... objects) {
+		print(objects);
+		writer.println();
+	}
+ 
+	public void close() {
+		writer.close();
+	}
+}   
